a2bb02d555506c9f7e83e8c7871c0aef2324d9b4
xala3pa <salazar3pa@gmail.com>
2016-12-27 18:53:54 +0100

    add properties to book and author domain objets

--- catalog-service/grails-app/domain/catalog/service/Author.groovy
+++ catalog-service/grails-app/domain/catalog/service/Author.groovy
@@ -1,7 +1,13 @@
 package catalog.service
 
 class Author {
+    String name
+    String surname
+
+    static hasMany = [books: Book]
 
     static constraints = {
+        name blank: false
+        surname blank: false
     }
 }
--- catalog-service/grails-app/domain/catalog/service/Book.groovy
+++ catalog-service/grails-app/domain/catalog/service/Book.groovy
@@ -1,7 +1,16 @@
 package catalog.service
 
 class Book {
+    String title
+    long pages
+    String isbn
+
+    static belongsTo = Author
+    static hasMany = [authors: Author]
 
     static constraints = {
+        title blank: false
+        pages size: 1..1000, blank: false
+        isbn blank: false
     }
 }
