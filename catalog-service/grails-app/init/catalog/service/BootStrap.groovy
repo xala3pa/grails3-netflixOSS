284805924bf30e7db693d4978ecc4242e85d399b
xala3pa <salazar3pa@gmail.com>
2016-12-28 00:01:41 +0100

    some data to test the catalog service

--- catalog-service/grails-app/init/catalog/service/BootStrap.groovy
+++ catalog-service/grails-app/init/catalog/service/BootStrap.groovy
@@ -3,6 +3,9 @@ package catalog.service
 class BootStrap {
 
     def init = { servletContext ->
+        new Author(name: "Glen and Peter", surname: "Smith and Ledbrook")
+                .addToBooks(new Book(title: "Grails in acton", pages: 576, isbn: 9781617290961))
+                .save()
     }
     def destroy = {
     }
