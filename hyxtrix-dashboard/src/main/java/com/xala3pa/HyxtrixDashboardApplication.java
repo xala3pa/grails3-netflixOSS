3d85dd80eacfe0d9a1e0586466f3670af9d29cea
xala3pa <salazar3pa@gmail.com>
2017-01-06 19:13:41 +0100

    add service discovery support to the project

--- hyxtrix-dashboard/src/main/java/com/xala3pa/HyxtrixDashboardApplication.java
+++ hyxtrix-dashboard/src/main/java/com/xala3pa/HyxtrixDashboardApplication.java
@@ -2,9 +2,11 @@ package com.xala3pa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableHystrixDashboard
 public class HyxtrixDashboardApplication {
 
