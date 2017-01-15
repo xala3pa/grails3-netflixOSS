cae3110eafea983191475c6baa1d55f06bb60672
xala3pa <salazar3pa@gmail.com>
2017-01-14 22:06:55 -0800

    add service to Eureka

--- bookStore-service/grails-app/init/bookstore/service/Application.groovy
+++ bookStore-service/grails-app/init/bookstore/service/Application.groovy
@@ -2,7 +2,9 @@ package bookstore.service
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
