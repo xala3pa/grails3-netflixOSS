ce88d12f3295b6edb5500ee2478a2fff49802c89
xala3pa <salazar3pa@gmail.com>
2017-02-10 17:56:43 +0100

    add routing and filtering requests capabilities

---
+++ proxyGateway/src/main/java/com/xala3pa/ProxyGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.xala3pa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyGatewayApplication.class, args);
+	}
+}
---
+++ proxyGateway/src/test/java/com/xala3pa/ProxyGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xala3pa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProxyGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
