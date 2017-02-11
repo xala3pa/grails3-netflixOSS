0157a46fa547ca9d76ba48916ff23d096c13c9bc
xala3pa <salazar3pa@gmail.com>
2017-02-11 11:44:01 +0100

    Enable Zuul proxy

--- proxyGateway/src/main/java/com/xala3pa/ProxyGatewayApplication.java
+++ proxyGateway/src/main/java/com/xala3pa/ProxyGatewayApplication.java
@@ -2,7 +2,9 @@ package com.xala3pa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ProxyGatewayApplication {
 
