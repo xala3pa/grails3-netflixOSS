5efbd1e3df8506c5eb698d4b6af71442c5261c7c
xala3pa <salazar3pa@gmail.com>
2017-02-12 10:46:01 +0100

    add config server support dependency

--- proxyGateway/src/main/java/com/xala3pa/ProxyGatewayApplication.java
+++ proxyGateway/src/main/java/com/xala3pa/ProxyGatewayApplication.java
@@ -10,7 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class ProxyGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProxyGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyGatewayApplication.class, args);
+    }
+
 }
