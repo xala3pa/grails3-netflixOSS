8529a6c6f4cf317ea2f86e326bd81bf7c6305bbd
xala3pa <salazar3pa@gmail.com>
2017-02-12 12:47:56 +0100

    Add supprt for service descovery

--- proxyGateway/src/main/java/com/xala3pa/ProxyGatewayApplication.java
+++ proxyGateway/src/main/java/com/xala3pa/ProxyGatewayApplication.java
@@ -2,11 +2,13 @@ package com.xala3pa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ProxyGatewayApplication {
 
