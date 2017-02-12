e3e584fa18d3b57cc782fb7b87f402b9cdef125f
xala3pa <salazar3pa@gmail.com>
2017-02-12 10:38:05 +0100

    Enable service discovery support

--- proxyGateway/src/main/java/com/xala3pa/ProxyGatewayApplication.java
+++ proxyGateway/src/main/java/com/xala3pa/ProxyGatewayApplication.java
@@ -3,8 +3,10 @@ package com.xala3pa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProxyGatewayApplication {
 
